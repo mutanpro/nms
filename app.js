5cc50863f1439c755b4a80317430f683cf236f62
mutanpro <mutanpro@gmail.com>
2021-07-26 09:26:22 +0700

    tambah host

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ const session = expressSession({
     },
     store: new MySQLStore({
         config: {
+            host: process.env.DATABASE_HOST,
             user: process.env.DATABASE_USER,
             password: process.env.DATABASE_PASSWORD,
             database: process.env.DATABASE_NAME
